0ffb09f086f9e8d5aba60dc10cb7bd19d9c97035
vermucht <vermucht@gmail.com>
2023-07-11 01:21:51 +0200

    Simple Employee REST service

--- src/main/java/com/github/vermucht/smth/SmthApplication.java
+++ src/main/java/com/github/vermucht/smth/SmthApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-//@SpringBootApplication(exclude = {RepositoryRestMvcAutoConfiguration.class})
 public class SmthApplication {
 
     public static void main(String[] args) {
