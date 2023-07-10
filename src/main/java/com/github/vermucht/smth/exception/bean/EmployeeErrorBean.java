6d6dc36201a6e226b065072a374bbc402755e31c
Aleksei Sapozhnikov <vermucht@gmail.com>
2023-07-11 01:43:21 +0200

    Employee rest (#6)

---
+++ src/main/java/com/github/vermucht/smth/exception/bean/EmployeeErrorBean.java
@@ -0,0 +1,18 @@
+package com.github.vermucht.smth.exception.bean;
+
+public class EmployeeErrorBean {
+
+    private String info;
+
+    public EmployeeErrorBean(String info) {
+        this.info = info;
+    }
+
+    public String getInfo() {
+        return info;
+    }
+
+    public void setInfo(String info) {
+        this.info = info;
+    }
+}
