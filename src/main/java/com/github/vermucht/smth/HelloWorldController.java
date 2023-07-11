62473ed8c0f544b1079e4d4ed11048493538c071
vermucht <vermucht@gmail.com>
2023-07-11 02:48:30 +0200

    simple change in non-ignored path

--- src/main/java/com/github/vermucht/smth/HelloWorldController.java
+++ src/main/java/com/github/vermucht/smth/HelloWorldController.java
@@ -1,6 +1,5 @@
 package com.github.vermucht.smth;
 
-import com.github.vermucht.smth.repository.EmployeeRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,9 +7,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloWorldController {
 
-    public HelloWorldController(EmployeeRepository employeeRepository) {
+    public HelloWorldController() {
     }
 
+    // Change
     @GetMapping("/hello")
     public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
         return "Hello, %s".formatted(name);
