0229467b5182ef92b2048fe9a8427c7981419c36
vermucht <vermucht@gmail.com>
2023-07-20 17:29:18 +0200

    Adding testing env variables to test

--- src/test/java/com/github/vermucht/smth/SmthApplicationTests.java
+++ src/test/java/com/github/vermucht/smth/SmthApplicationTests.java
@@ -1,5 +1,6 @@
 package com.github.vermucht.smth;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,4 +9,11 @@ class SmthApplicationTests {
 
   @Test
   void contextLoads() {}
+
+  /** Checks if all needed environment variables are set. Appeared on studying GitHub Actions. */
+  @Test
+  void envVarIsSet() {
+    String envVar = System.getenv("ENV_VAR");
+    Assertions.assertTrue(envVar != null && envVar.length() > 0);
+  }
 }
