702855d5e2d2d7307ee150485e6086f1fb767fe6
vermucht <vermucht@gmail.com>
2023-07-20 17:44:18 +0200

    Do not check ENV_VAR if not directed to do that

--- src/test/java/com/github/vermucht/smth/SmthApplicationTests.java
+++ src/test/java/com/github/vermucht/smth/SmthApplicationTests.java
@@ -13,6 +13,10 @@ class SmthApplicationTests {
   /** Checks if all needed environment variables are set. Appeared on studying GitHub Actions. */
   @Test
   void envVarIsSet() {
+    String isCheck = System.getenv("IS_TEST_ENVIRONMENT");
+    if (!"true".equals(isCheck)) {
+      return;
+    }
     String envVar = System.getenv("ENV_VAR");
     Assertions.assertTrue(envVar != null && envVar.length() > 0);
   }
