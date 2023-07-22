fe0a726c2b1774e5d710672d68990f613196801e
vermucht <vermucht@gmail.com>
2023-07-22 21:30:30 +0200

    adding inputs

--- .github/actions/example-js-action/main.js
+++ .github/actions/example-js-action/main.js
@@ -2,9 +2,9 @@ import core from "@actions/core";
 
 function run() {
     core.notice('### Running MAIN logic');
-    const var1 = core.getInput('var1')
-    const var2 = core.getInput('var2');
-    const number = core.getInput('number') || -1;
+    const var1 = core.getInput('var1', {required: true})
+    const var2 = core.getInput('var2', {required: true});
+    const number = core.getInput('number', {required: true});
     const guessed = number.toString() === "10";
     core.notice(`var1 = ${var1}`)
     core.notice(`var2 = ${var2}`)
