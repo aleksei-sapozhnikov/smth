aa09602b1bead195a60595a804819ca833ddf446
vermucht <vermucht@gmail.com>
2023-07-22 23:00:01 +0200

    trying to add java docker action

--- .github/actions/example-docker-action/src/main/java/Main.java
+++ .github/actions/example-docker-action/src/main/java/Main.java
@@ -1,5 +1,14 @@
 public class Main {
   public static void main(String[] args) {
-    System.out.println(System.getenv());
+    // printing values
+    System.out.println("VAR1 = " + System.getenv("VAR1"));
+    System.out.println("VAR2 = " + System.getenv("VAR2"));
+    // simple logic with number input
+    String strNumber = System.getenv("NUMBER");
+    System.out.println("NUMBER = " + strNumber);
+    boolean guessed = "10".equals(strNumber);
+    System.out.println("guessed = " + guessed);
+    // output
+    System.out.println("guessed-number=" + guessed);
   }
 }
