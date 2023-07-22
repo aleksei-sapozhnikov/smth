753dbd4c826129ce8afc5809b641a97c7ac3b253
vermucht <vermucht@gmail.com>
2023-07-22 20:31:28 +0200

    add JS action

--- .github/actions/example-js-action/main.js
+++ .github/actions/example-js-action/main.js
@@ -3,7 +3,7 @@ import github from "@actions/github";
 import exec from "@actions/exec";
 
 function run() {
-    core.notice('Starting MAIN action logic');
+    core.notice('### Starting MAIN action logic');
 }
 
 run()
